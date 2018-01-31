a3c1ecc9216feacac6a05c3ccfa5b4d4e5123c17
orgadk <orgadk@gmail.com>
2018-01-31 20:48:54 +0200

    Update shutdown-vms.sh
    add zone and shutdown work

--- shutdown-vms.sh
+++ shutdown-vms.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for k in us-central1-a us-central1-b us-central1-f; do
+for k in us-central1-a us-central1-b us-central1-c us-central1-f ; do
 	for i in $(gcloud compute instances list --zones $k| awk 'NR>1 {print $1}'); do
 		gcloud compute instances stop $i -q --zone "$k" &
 	done;
 done;
+gcloud compute instances stop work --zone europe-west3-a 	
