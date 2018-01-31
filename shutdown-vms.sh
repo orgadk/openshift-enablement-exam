b7fa7493044a155655b3e840e73a683d425c3851
orgadk <orgadk@gmail.com>
2018-01-31 20:33:28 +0200

    Update shutdown-vms.sh

--- shutdown-vms.sh
+++ shutdown-vms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for k in us-central1-a us-central1-b us-central1-f; do
 	for i in $(gcloud compute instances list --zones $k| awk 'NR>1 {print $1}'); do
-		gcloud compute instances shutdown $i -q --zone "$k" &
+		gcloud compute instances stop $i -q --zone "$k" &
 	done;
 done;
