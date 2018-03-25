b96414adbb85249839db5ca4d9c897b11fdbb7da
orgadk <orgadk@gmail.com>
2018-03-25 12:42:18 +0300

    change location  "yum update -y"

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -5,7 +5,8 @@ set -e
 ansible nodes -b -i hosts -m shell -a "yum install -y subscription-manager && subscription-manager clean" 
 ansible nodes -b -i hosts -m shell -a "subscription-manager register --username=$RHN_USERNAME --password=$RHN_PASSWORD && subscription-manager attach --pool=$RHN_SUB_POOL && subscription-manager refresh" 
 ansible nodes -b -i hosts -m shell -a "subscription-manager repos --disable='*' && subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.7-rpms --enable=rhel-7-fast-datapath-rpms"
-ansible nodes -b -i hosts -m shell -a "yum update -y && yum install -y docker-1.12.6 wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
+ansible nodes -b -i hosts -m shell -a "yum install -y docker-1.12.6 wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
+ansible nodes -b -i hosts -m shell -a "yum update -y"
 ansible 'nodes:!masters' -i hosts -b -m copy -a "src=docker-storage-setup dest=/etc/sysconfig/docker-storage-setup"
 ansible nodes -b -i hosts -m shell -a "yum install -y atomic-openshift-utils"
 #this is non-idempotent
