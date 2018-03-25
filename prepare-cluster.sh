47f82ed73d06cf9a8074ada1636cafe490274713
orgadk <orgadk@gmail.com>
2018-03-25 15:17:59 +0300

    copy /etc/sysconfig/docker-storage-setup to masters
    copy /etc/sysconfig/docker-storage-setup to masters

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -7,7 +7,7 @@ ansible nodes -b -i hosts -m shell -a "subscription-manager register --username=
 ansible nodes -b -i hosts -m shell -a "subscription-manager repos --disable='*' && subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.7-rpms --enable=rhel-7-fast-datapath-rpms"
 ansible nodes -b -i hosts -m shell -a "yum install -y docker-1.12.6 wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
 ansible nodes -b -i hosts -m shell -a "yum update -y"
-ansible 'nodes:!masters' -i hosts -b -m copy -a "src=docker-storage-setup dest=/etc/sysconfig/docker-storage-setup"
+ansible nodes -i hosts -b -m copy -a "src=docker-storage-setup dest=/etc/sysconfig/docker-storage-setup"
 ansible nodes -b -i hosts -m shell -a "yum install -y atomic-openshift-utils"
 #this is non-idempotent
 ansible 'nodes:!masters' -i hosts -b -m shell -a "docker-storage-setup"
